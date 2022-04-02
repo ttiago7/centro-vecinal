328258d63ea2910291b95157c9da54e71448cb83
facu410 <facundito410@gmail.com>
2022-04-02 18:12:19 -0300

    public navbar finish & hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import "./Hero.css";
+
+
+const hero = () => {
+
+return(
+        <div className="Herocontainer">
+                <h1>Hola Mundo</h1>
+        </div>
+    
+)
+}
+
+export default hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import NavbarAdmin from "../components/Navbar/NavbarAdmin";
+import NavbarPublic from "../components/Navbar/Onlynavbar";
+import Hero from "../components/Hero/Hero";
 
 function Home() {
   return (
     <>
       <NavbarAdmin />
+      <NavbarPublic/>
+      <Hero/> 
     </>
   );
 }
